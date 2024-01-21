e76476c7e99956877c8ec0652c7bce7c27975ec7
null <108763797+userNu11ified@users.noreply.github.com>
2024-01-21 14:17:21 +0100

    feat: :tada: Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import ViteExpress from "vite-express";
+
+const mode = process.argv[2] ?? "development";
+if (mode !== "development" && mode !== "production")
+	throw new Error("Invalid environment mode set!");
+
+const PORT_NUMBER = 3000;
+const app = express();
+
+ViteExpress.config({ viteConfigFile: "./client/vite.config.ts", mode: mode });
+ViteExpress.listen(app, PORT_NUMBER, () => {
+	console.log("Server started!");
+});
